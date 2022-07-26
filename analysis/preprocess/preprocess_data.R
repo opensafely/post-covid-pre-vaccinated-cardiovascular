67d91a65a7ac86c6524e39f3f1f71b886e15d70c
RochelleKnight <rochelle.knight@bristol.ac.uk>
2022-07-26 15:11:52 +0100

    Update preprocess_data.R

--- analysis/preprocess/preprocess_data.R
+++ analysis/preprocess/preprocess_data.R
@@ -5,10 +5,6 @@
 # Input: output/input.feather
 # Output: output/
 #
-# Author(s): Rachel Denholm,  Kurt Taylor
-#
-# Date last updated: 
-#
 ##################################################################################
 
 # Load libraries ---------------------------------------------------------------
@@ -22,44 +18,18 @@ library(lubridate)
 fs::dir_create(here::here("output", "not-for-review"))
 fs::dir_create(here::here("output", "review"))
 
-# Define parameters ------------------------------------------------------------
+# Define general start date and end date
+cohort_start_date = as.Date("2020-01-01")
+cohort_end_date = as.Date("2021-06-18") # General End date: 2021-06-18 (date last JCVI group eligible for vaccination - Decision on Jan 18th 2022)
 
-## Study start date
-study_start <- "2020-01-01"
 
 ## Load dataset
 df <- arrow::read_feather(file = "output/input.feather")
 
-# create vars -------------------------------------------------------------
-
-# vars could not be created in common vars file
-df <- df %>% mutate(tmp_out_count_t2dm = tmp_out_count_t2dm_snomed + tmp_out_count_t2dm_hes,
-                    tmp_out_count_t1dm = tmp_out_count_t1dm_snomed + tmp_out_count_t1dm_hes)
-      # cholesterol ratio
-      # impossible a ratio < 1 because HDL cholesterol cannot be higher than total cholesterol
-      # not possible for ratio to be above 50 because of criteria above for cholesterol levels
-
-
-print("Diabetes count variables created successfully")
-
-# remove biologically implausible TC/HDL ratio values: https://doi.org/10.1093/ije/dyz099
-# Remove TC < 1.75 or > 20 
-# remove HDL < 0.4 or > 5
+## Define cohort start and end dates
 df <- df %>%
-  mutate(tmp_cov_num_cholesterol = replace(tmp_cov_num_cholesterol, tmp_cov_num_cholesterol < 1.75 | tmp_cov_num_cholesterol > 20, NA),
-         tmp_cov_num_hdl_cholesterol = replace(tmp_cov_num_hdl_cholesterol, tmp_cov_num_hdl_cholesterol < 0.4 | tmp_cov_num_hdl_cholesterol > 5, NA)) %>%
-  mutate(cov_num_tc_hdl_ratio = tmp_cov_num_cholesterol / tmp_cov_num_hdl_cholesterol) %>%
-  mutate(cov_num_tc_hdl_ratio = replace(cov_num_tc_hdl_ratio, cov_num_tc_hdl_ratio > 50 | cov_num_tc_hdl_ratio < 1, NA))
-
-print("Cholesterol ratio after removing values")
-summary(df$cov_num_tc_hdl_ratio)
-
-# replace NaN and Inf with NA's (probably only an issue with dummy data)
-df$cov_num_tc_hdl_ratio[is.nan(df$cov_num_tc_hdl_ratio)] <- NA
-df$cov_num_tc_hdl_ratio[is.infinite(df$cov_num_tc_hdl_ratio)] <- NA
-print("Cholesterol ratio before removing values")
-summary(df$cov_num_tc_hdl_ratio)
-
+  mutate(index_date = cohort_start_date,
+         cohort_end_date = cohort_end_date)
 
 # QC for consultation variable
 # max to 365 (average of one per day)
@@ -77,19 +47,16 @@ summary(df$cov_num_consulation_rate)
 
 df <- df %>%
   mutate(cov_bin_obesity = ifelse(cov_bin_obesity == TRUE | cov_cat_bmi_groups == "Obese", TRUE, FALSE)) %>%
-  dplyr::select(- cov_num_bmi)
+  dplyr::select(- c(cov_num_bmi,cov_num_bmi_date_measured,cov_cat_bmi_groups))
 
 # Format columns -----------------------------------------------------
 # dates, numerics, factors, logicals
 
-df <- df %>%
-  rename(tmp_out_max_hba1c_mmol_mol_date = tmp_out_num_max_hba1c_date,
-         tmp_out_bmi_date_measured = cov_num_bmi_date_measured) %>%
-  mutate(across(contains('_date'), ~ as.Date(as.character(.)))) %>%
-  mutate(across(contains('_birth_year'), ~ format(as.Date(.), "%Y"))) %>%
-  mutate(across(contains('_num'), ~ as.numeric(.))) %>%
-  mutate(across(contains('_cat'), ~ as.factor(.))) %>%
-  mutate(across(contains('_bin'), ~ as.logical(.)))
+df <- df %>% mutate(across(contains('_date'), ~ as.Date(as.character(.))),
+                    across(contains('_birth_year'), ~ format(as.Date(.), "%Y")),
+                    across(contains('_num'), ~ as.numeric(.)),
+                    across(contains('_cat'), ~ as.factor(.)),
+                    across(contains('_bin'), ~ as.factor(.)))
 
 print("Columns formatted successfully")
 
@@ -105,41 +72,14 @@ df <- df %>%
                          ifelse(is.na(exp_date_covid19_confirmed), "no_infection", NA)))) %>%
   mutate(across(sub_cat_covid19_hospital, factor))
 
-# Define diabetes outcome (using Sophie Eastwood algorithm) ----------------------------
-
-# define variables needed for diabetes algorithm 
-
-df <- df %>% 
-  mutate(tmp_out_year_first_diabetes_diag = format(tmp_out_date_first_diabetes_diag,"%Y")) %>%
-  mutate(tmp_out_year_first_diabetes_diag = as.integer(tmp_out_year_first_diabetes_diag),
-         age_1st_diag = tmp_out_year_first_diabetes_diag - qa_num_birth_year) %>%
-  mutate(age_1st_diag = replace(age_1st_diag, which(age_1st_diag < 0), NA)) %>% # assign negative ages to NA)
-  mutate(age_under_35_30_1st_diag = ifelse(!is.na(age_1st_diag) &
-                                             (age_1st_diag < 35 & 
-                                                (cov_cat_ethnicity == 1 | cov_cat_ethnicity == 2  | cov_cat_ethnicity == 5)) | 
-                                             (age_1st_diag < 30), "Yes", "No")) %>%
-  # HBA1C date var - earliest date for only those with >=47.5
-  mutate(hba1c_date_step7 = as_date(case_when(tmp_out_num_max_hba1c_mmol_mol >= 47.5 ~ pmin(tmp_out_max_hba1c_mmol_mol_date, na.rm = TRUE))),
-         # process codes - this is taking the first process code date in those individuals that have 5 or more process codes
-         over5_pocc_step7 = as_date(case_when(tmp_out_count_poccdm_snomed >= 5 ~ pmin(out_date_poccdm, na.rm = TRUE))))
-
-print("COVID-19 and diabetes variables needed for algorithm created successfully")
-
-# Define diabetes outcome (using Sophie Eastwood algorithm) ----------------------------
-
-scripts_dir <- "analysis/preprocess"
-source(file.path(scripts_dir,"diabetes_algorithm.R"))
-df <- diabetes_algo(df)
-print("Diabetes algorithm run successfully")
 
 # Restrict columns and save analysis dataset ---------------------------------
 
 df1 <- df %>% 
-  dplyr::select(- vax_jcvi_age_1, - vax_jcvi_age_2) %>% #  remove JCVI variables
+  dplyr::select(- vax_jcvi_age_1, - vax_jcvi_age_2, vax_cat_jcvi_group) %>% #  remove JCVI variables
   # select patient id, death date and variables: subgroups, exposures, outcomes, covariates, quality assurance and vaccination
-  # need diabetes "step" variables for flowchart (diabetes_flowchart.R)
-  dplyr::select(patient_id, death_date,
-                contains(c("sub_", "exp_", "out_", "cov_", "qa_", "vax_", "step"))) %>%
+  dplyr::select(patient_id, death_date,index_date,cohort_end_date,
+                contains(c("sub_", "exp_", "out_", "cov_", "qa_", "vax_"))) %>%
   dplyr::select(-contains("df_out_")) %>%
   dplyr::select(-contains("tmp_"))
 
@@ -155,17 +95,15 @@ saveRDS(df1, file = paste0("output/input.rds"))
 
 print("Dataset saved successfully")
 
-# Restrict columns and save Venn diagram input dataset -----------------------
+# Save Venn diagram input dataset -----------------------
+df <- df %>% select(starts_with(c("patient_id","tmp_out_date","out_date")))
 
-# df2 <- df %>% 
-#   dplyr::select(patient_id,
-#                 starts_with(c("out_")))
+# Describe data --------------------------------------------------------------
 
-# SAVE
-## create folders for outputs
-# fs::dir_create(here::here("output", "venn"))
+sink(paste0("output/not-for-review/describe_venn_input.txt"))
+print(Hmisc::describe(df))
+sink()
+
 saveRDS(df, file = paste0("output/venn.rds"))
 
 print("Venn dataset saved successfully")
-
-# END
