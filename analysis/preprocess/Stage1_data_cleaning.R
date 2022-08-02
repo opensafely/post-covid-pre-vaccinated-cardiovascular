39f66d4a126bea192b30bc4db993d2f968f92cf8
RochelleKnight <rochelle.knight@bristol.ac.uk>
2022-08-02 21:05:13 +0100

    Update Stage1_data_cleaning.R

--- analysis/preprocess/Stage1_data_cleaning.R
+++ analysis/preprocess/Stage1_data_cleaning.R
@@ -173,7 +173,7 @@ cohort_flow[nrow(cohort_flow)+1,] <- c(nrow(input), as.numeric(QA_summary[7,2]) 
 
 # Inclusion criteria 1: Alive on the first day of follow up
 
-input <- input %>% filter(index_date < death_date)
+input <- input %>% filter(index_date < death_date | is.na(death_date))
 cohort_flow[nrow(cohort_flow)+1,] <- c(nrow(input), as.numeric(cohort_flow[2,1]) - nrow(input),"Criteria 1 (Inclusion): Alive on the first day of follow up") # Feed into the cohort flow
 
 # Inclusion criteria 2: Known age between 18 and 110 on 01/01/2020 
