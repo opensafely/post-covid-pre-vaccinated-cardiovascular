1e4b8be4fea38773f820f11c47e72adb8447a800
Genevieve Cezard <gic30@medschl.cam.ac.uk>
2023-10-02 13:41:24 +0100

    Change code days_covid19_to_hospitalisation.R

--- analysis/descriptives/days_covid19_to_hospitalisation.R
+++ analysis/descriptives/days_covid19_to_hospitalisation.R
@@ -23,13 +23,13 @@ input <- input %>%
   mutate(days_Covid19_to_hospitalisation = 
            ifelse(sub_cat_covid19_hospital == "hospitalised", input$sub_date_covid19_hospital - input$exp_date_covid19_confirmed, NA)
   )
-table <- input %>% dplyr::count(days_Covid19_to_hospitalisation)
+table_N_days <- input %>% dplyr::count(days_Covid19_to_hospitalisation)
 
 # Add in suppression for counts <=5
-table_for_disclosure <- table %>%
+table_N_days_for_disclosure <- table_N_days %>%
   mutate(n_people = 
            ifelse(n >5, n, "[Redacted]")
   ) %>%
   select(days_Covid19_to_hospitalisation,n_people)
 
-write.csv(table_for_disclosure, file=paste0("output/review/descriptives/days_COVID19_to_hospitalisation_",cohort_name, ".csv"), row.names = F)
+write.csv(table_N_days_for_disclosure, file=paste0("output/review/descriptives/days_COVID19_to_hospitalisation_",cohort_name, ".csv"), row.names = F)
